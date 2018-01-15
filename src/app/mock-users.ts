c84fc330cd371725aceb61c0f67c22c0307b16a0
Danny Smith <5522646+nomorethirst@users.noreply.github.com>
2018-01-15 17:50:55 -0600

    Add mock-users to match mock-tweets

---
+++ src/app/mock-users.ts
@@ -0,0 +1,223 @@
+import {User} from './user';
+
+export const USERS: User[] = [
+  {
+    username: 'krastrick3',
+    joined: 1516058653093,
+    profile:
+      {
+        firstName: 'Krispin',
+        lastName: 'Rastrick',
+        email: 'krastrick3@amazonaws.com',
+        phone: '316-399-3871'
+      }
+  },
+  {
+    username: 'mbruhn4',
+    joined: 1516058653093,
+    profile:
+      {
+        firstName: 'Merl',
+        lastName: 'Bruhn',
+        email: 'mbruhn4@chron.com',
+        phone: '813-100-2898'
+      }
+  },
+  {
+    username: 'mlambourn5',
+    joined: 1516058653093,
+    profile:
+      {
+        firstName: 'Mayer',
+        lastName: 'Lambourn',
+        email: 'mlambourn5@bloglines.com',
+        phone: '136-683-4147'
+      }
+  },
+  {
+    username: 'ddunster2',
+    joined: 1516058653093,
+    profile:
+      {
+        firstName: 'Dory',
+        lastName: 'Dunster',
+        email: 'ddunster2@vkontakte.ru',
+        phone: '943-120-8154'
+      }
+  },
+  {
+    username: 'cbrugger0',
+    joined: 1516058653093,
+    profile:
+      {
+        firstName: 'Carey',
+        lastName: 'Brugger',
+        email: 'cbrugger0@guardian.co.uk',
+        phone: '440-364-1603'
+      }
+  },
+  {
+    username: 'ulukock1',
+    joined: 1516058653093,
+    profile:
+      {
+        firstName: 'Ulises',
+        lastName: 'Lukock',
+        email: 'ulukock1@mapquest.com',
+        phone: '464-153-6253'
+      }
+  },
+  {
+    username: 'rmonnelly6',
+    joined: 1516058653216,
+    profile:
+      {
+        firstName: 'Rudiger',
+        lastName: 'Monnelly',
+        email: 'rmonnelly6@craigslist.org',
+        phone: '599-805-3275'
+      }
+  },
+  {
+    username: 'bstothard7',
+    joined: 1516058653216,
+    profile:
+      {
+        firstName: 'Bill',
+        lastName: 'Stothard',
+        email: 'bstothard7@forbes.com',
+        phone: '641-580-2301'
+      }
+  },
+  {
+    username: 'dpeplay9',
+    joined: 1516058653215,
+    profile:
+      {
+        firstName: 'Dawn',
+        lastName: 'Peplay',
+        email: 'dpeplay9@g.co',
+        phone: '793-727-1394'
+      }
+  },
+  {
+    username: 'gpeniman8',
+    joined: 1516058653215,
+    profile:
+      {
+        firstName: 'Gardner',
+        lastName: 'Peniman',
+        email: 'gpeniman8@histats.com',
+        phone: '734-604-3083'
+      }
+  },
+  {
+    username: 'lminshullb',
+    joined: 1516058653215,
+    profile:
+      {
+        firstName: 'Lorilee',
+        lastName: 'Minshull',
+        email: 'lminshullb@sakura.ne.jp',
+        phone: '482-423-0943'
+      }
+  },
+  {
+    username: 'ckynfortha',
+    joined: 1516058653218,
+    profile:
+      {
+        firstName: 'Corbett',
+        lastName: 'Kynforth',
+        email: 'ckynfortha@miibeian.gov.cn',
+        phone: '857-287-8771'
+      }
+  },
+  {
+    username: 'mjanssenc',
+    joined: 1516058653235,
+    profile:
+      {
+        firstName: 'Madelon',
+        lastName: 'Janssen',
+        email: 'mjanssenc@bing.com',
+        phone: '425-940-4596'
+      }
+  },
+  {
+    username: 'zsaywelle',
+    joined: 1516058653235,
+    profile:
+      {
+        firstName: 'Zak',
+        lastName: 'Saywell',
+        email: 'zsaywelle@freewebs.com',
+        phone: '303-103-2321'
+      }
+  },
+  {
+    username: 'crotchellg',
+    joined: 1516058653235,
+    profile:
+      {
+        firstName: 'Cad',
+        lastName: 'Rotchell',
+        email: 'crotchellg@chron.com',
+        phone: '714-753-8897'
+      }
+  },
+  {
+    username: 'elundyd',
+    joined: 1516058653235,
+    profile:
+      {
+        firstName: 'Ellen',
+        lastName: 'Lundy',
+        email: 'elundyd@telegraph.co.uk',
+        phone: '464-605-1135'
+      }
+  },
+  {
+    username: 'fmckeefryh',
+    joined: 1516058653237,
+    profile:
+      {
+        firstName: 'Flem',
+        lastName: 'McKeefry',
+        email: 'fmckeefryh@reddit.com',
+        phone: '198-613-9260'
+      }
+  },
+  {
+    username: 'mbrendef',
+    joined: 1516058653241,
+    profile:
+      {
+        firstName: 'Moritz',
+        lastName: 'Brende',
+        email: 'mbrendef@patch.com',
+        phone: '670-198-5476'
+      }
+  },
+  {
+    username: 'sbutchersj',
+    joined: 1516058653250,
+    profile:
+      {
+        firstName: 'Shaun',
+        lastName: 'Butchers',
+        email: 'sbutchersj@nature.com',
+        phone: '742-285-0552'
+      }
+  },
+  {
+    username: 'oebanksi',
+    joined: 1516058653250,
+    profile:
+      {
+        firstName: 'Ofella',
+        lastName: 'Ebanks',
+        email: 'oebanksi@yelp.com',
+        phone: '647-296-8929'
+      }
+  }];
